cfcb424b690ef4978908170f0bd2e51a35b8fe04
Kael Barbosa Lopes <kaelbds@hotmail.com>
2021-06-11 15:27:32 -0400

    funcionalidade-botao-alter

--- script/script.js
+++ script/script.js
@@ -1,23 +1,24 @@
-
 function insert(num) {
     var numero = document.getElementById('operacao').innerHTML;
     document.getElementById('operacao').innerHTML = numero + num;
 }
-
 function clean(){
     document.getElementById('operacao').innerHTML = "";
     document.getElementById('resultado').innerHTML = "";
-
 }
-
 function back(){
     var resultado = document.getElementById('operacao').innerHTML;
     document.getElementById('operacao').innerHTML = resultado.substring(0, resultado.length -1);
 }
-
 function calcular(){
     var resultado = document.getElementById('operacao').innerHTML;
-    if(operacao){
-        document.getElementById('resultado').innerHTML = eval(resultado);
-    }
+    if(operacao) document.getElementById('resultado').innerHTML = eval(resultado);
 }
+function alter(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    parseFloat(resultado);
+    resultado *= -1;
+    document.
+    getElementById('resultado').innerHTML = resultado;
+}
+
