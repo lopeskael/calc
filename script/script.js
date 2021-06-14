f662c01c73c14d535d9fe7931a42bb2dd8e89fd7
lopeskael <kaelbds@hotmail.com>
2021-06-14 18:17:34 -0400

    melhorando layout

--- script/script.js
+++ script/script.js
@@ -1,32 +1,31 @@
 function insert(num) {
     var numero = document.getElementById('operacao').innerHTML;
     document.getElementById('operacao').innerHTML = numero + num;
-
 }
-function clean(){
+function clean() {
     document.getElementById('operacao').innerHTML = "";
     document.getElementById('resultado').innerHTML = "";
 }
-function back(){
+function back() {
     var resultado = document.getElementById('operacao').innerHTML;
-    document.getElementById('operacao').innerHTML = resultado.substring(0, resultado.length -1);
+    document.getElementById('operacao').innerHTML = resultado.substring(0, resultado.length - 1);
 }
-function calcular(){
+function calcular() {
     var resultado = document.getElementById('operacao').innerHTML;
-    if(operacao) document.getElementById('resultado').innerHTML = eval(resultado);
+    if (operacao) document.getElementById('resultado').innerHTML = eval(resultado);
 }
-function alter(){
+function alter() {
     let resultado = document.getElementById('resultado').innerHTML;
     parseFloat(resultado);
     resultado *= -1;
     document.getElementById('resultado').innerHTML = resultado;
 }
 
-function percento(){
+function percento() {
     var percent = document.getElementById('operacao').innerHTML;
     var percent1 = parseFloat(percent, 0);
     var percent2 = percent.slice(-1);
-    parseFloat(percent2); 
-    percent2 = percent2/100;
-    document.getElementById('operacao').innerHTML =  percent1+'*'+percent2;
+    parseFloat(percent2);
+    percent2 = percent2 / 100;
+    document.getElementById('operacao').innerHTML = percent1 + '*' + percent2;
 }
